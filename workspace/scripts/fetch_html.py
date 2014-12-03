bb3d2741c235470231dd2b873caabb6da4adc6c3
doronrk <doronrk@gmail.com>
2014-12-02 20:13:34 -0500

    improved fetch script

--- workspace/scripts/fetch_html.py
+++ workspace/scripts/fetch_html.py
@@ -4,8 +4,18 @@ import sys
 from selenium import webdriver
 import os
 import time
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import WebDriverException
+from selenium.common.exceptions import TimeoutException
+
+def ajax_complete(driver):
+    try:
+        return 0 == driver.execute_script("return jQuery.active")
+    except WebDriverException:
+        pass
 
 url_filename = '../product_urls.txt'
+error_url_filename = '../error_product_urls.txt'
 url_dir = '../product_html'
 
 if len(sys.argv) == 3:
@@ -30,7 +40,7 @@ for url_string in urls:
         continue
     try:
         driver.get(url_string)
-        time.sleep(3)
+        WebDriverWait(driver, 10).until(ajax_complete, "Timeout waiting for page to load")
         html_file = open(url_dir + '/' + domain + '.html',"w")
         html = driver.page_source
         html = html.encode('utf-8')
@@ -45,20 +55,23 @@ for url_string in urls:
         else:
             errors[e.code] = [url_string]
         print '\t', e.code, 'error'
+    except TimeoutException as e:
+        if 'timeout_exception' in errors:
+            errors['timeout_exception'].append(url_string)
+        else:
+            errors['timeout_exception'] = [url_string]
+        print 'timeout_exception'
 
 print 'number of succesful_fetches', num_succesful_fetches
 
 driver.close()
 
-
 if errors == dict():
     print 'no errors'
 else:
-    print 'errors'
+    error_url_file = open(error_url_filename,"w")
 
     for error_code, urls in errors.iteritems():
-        print 'code: ', error_code
+        error_url_file.write('\n\n' + error_code + '\n\n')
         for url in urls:
-            print '\t', url
-
-
+            error_url_file.write(url + '\n')
