e9e372d485de7b9c98cafba153c6e89bf86ff5a6
doronrk <doronrk@gmail.com>
2014-12-03 23:16:03 -0500

    IM SICK AND TIRED OF THIS SHIT

--- workspace/product_html_maya/www.newegg.com_files/utag_002.js
+++
@@ -1,7 +0,0 @@
-//tealium universal tag - utag.171 ut4.0.201407222235, Copyright 2014 Tealium.com Inc. All Rights Reserved.
-if(typeof utag.ut=="undefined"){utag.ut={};}
-utag.ut.libloader2=function(o,a,b,c,l){a=document;b=a.createElement('script');b.language='javascript';b.type='text/javascript';b.async=true;b.src=o.src;if(o.id){b.id=o.id}
-if(typeof o.cb=='function'){b.hFlag=0;b.onreadystatechange=function(){if((this.readyState=='complete'||this.readyState=='loaded')&&!b.hFlag){b.hFlag=1;o.cb();}};b.onload=function(){if(!b.hFlag){b.hFlag=1;o.cb();}};}
-l=o.loc||'head';c=a.getElementsByTagName(l)[0];if(c){if(l=='script'){c.parentNode.insertBefore(b,c);}else{c.appendChild(b);}
-utag.DB("Attach to "+l+": "+o.src);}};try{(function(id,loader){var u=utag.o[loader].sender[id]={};u.ev={'view':1};u.initialized=false;u.map={};u.extend=[];u.send=function(a,b){if(u.ev[a]||typeof u.ev.all!="undefined"){var c,d,e,f,i;u.data={};for(d in utag.loader.GV(u.map)){if(typeof b[d]!='undefined'){e=u.map[d].split(',');for(f=0;f<e.length;f++){u.data[e[f]]=b[d];}}}
-(function(){var s=document.createElement('script'),e=document.getElementsByTagName('script')[0];s.src='//d3cxv97fi8q177.cloudfront.net/foundation-A60088-19ec-4f7d-ba65-a67a3cb1a7491.js';s.type='text/javascript';s.async=true;e.parentNode.insertBefore(s,e);})();}};utag.o[loader].loader.LOAD(id);})('171','newegg.newegg.com');}catch(e){}
--- workspace/scripts/feature_extraction.py
+++ workspace/scripts/feature_extraction.py
@@ -4,15 +4,14 @@ import os
 import re
 
 def get_attr_dict(element_str):
-    print 'element_str', element_str
     soup = BeautifulSoup(element_str)
-    print 'soup', soup
-    print ''
     element = soup.html.body.find()
     attrs = element.attrs
     attr_dict = dict()
     for attr in attrs:
         value = element[attr]
+        if not type(value) == list:
+            value = [value]
         attr_dict[attr] = value
     return element, attr_dict
 
@@ -25,18 +24,22 @@ def label_element(element_str):
     element, attr_dict = get_attr_dict(element_str)
 
 def get_text_regex_from_element(element):
-    if (element.string == None):
+    text = element.string
+    #print 'text', text
+    if (text == None):
         return None
-    stripped_element_string = "".join(element.string.split())
+    stripped_element_string = "".join(text.split())
     escaped_stripped_element_string = re.escape(stripped_element_string)
     return re.compile(escaped_stripped_element_string)
 
-def get_matches_from_element_str(element_str, soup):
+def get_match_from_element_str(element_str, soup):
     element, attr_dict = get_attr_dict(element_str)
     element_text_regex = get_text_regex_from_element(element)
-    soup_matches = soup.find_all(text=element_text_regex, attrs=attr_dict)
+    soup_matches = soup.find_all(element.name, text=element_text_regex, attrs=attr_dict)
     # TODO: add selenium matches
-    return soup_matches
+    if soup_matches == []:
+        return None
+    return soup_matches[0]
 
 
 html_path = "../all_of_the_product_html/"
@@ -51,10 +54,10 @@ file_found_both_elements = set()
 file_found_only_price = set()
 file_found_only_image = set()
 file_found_neither = set()
-file_found_multiple_matches = set()
+
 
 for num, feature_element in enumerate(feature_elements):
-    print 'feature_element', feature_element
+    print num
     filename, price_element_str, image_element_str = feature_element.split('\t')
     if not (filename in html_filenames):
         files_not_in_directory.add(filename)
@@ -64,38 +67,39 @@ for num, feature_element in enumerate(feature_elements):
     html_file = open(filepath)
     soup = BeautifulSoup(html_file)
 
-    price_soup_matches = get_matches_from_element_str(price_element_str)
-    image_soup_matches = get_matches_from_element_str(image_element_str)
+    price_soup_match = get_match_from_element_str(price_element_str, soup)
+    image_soup_match = None
+    # only try and find it if it wasn't blank in the spread sheet
+    if len(image_element_str) > 1:
+        image_soup_match = get_match_from_element_str(image_element_str, soup)
 
-    if (len(price_soup_matches) == 1 and len(image_soup_matches) == 1):
+
+    if (price_soup_match != None and image_soup_match != None):
         file_found_both_elements.add(filename)
-    if (len(price_soup_matches) == 1 and len(image_soup_matches) == 0):
+    if (price_soup_match != None and image_soup_match == None):
         file_found_only_price.add(filename)
-    if (len(price_soup_matches) == 0 and len(image_soup_matches) == 1):
+    if (price_soup_match == None and image_soup_match != None):
         file_found_only_image.add(filename)
-    if (len(price_soup_matches) == 0 and len(image_soup_matches) == 0):
+    if (price_soup_match == None and image_soup_match == None):
         file_found_neither.add(filename)
-    if (len(price_soup_matches) > 1 or len(image_soup_matches) > 1):
-        file_found_multiple_matches.add(filename)
 
-print 'files_not_in_directory'
+
+print 'files_not_in_directory', len(files_not_in_directory)
 for f in files_not_in_directory:
     print '\t', f
-print 'file_found_both_elements'
+print 'file_found_both_elements', len(file_found_both_elements)
 for f in file_found_both_elements:
     print '\t', f
-print 'file_found_only_price'
+print 'file_found_only_price', len(file_found_only_price)
 for f in file_found_only_price:
     print '\t', f
-print 'file_found_only_image'
+print 'file_found_only_image', len(file_found_only_image)
 for f in file_found_only_image:
     print '\t', f
-print 'file_found_neither'
+print 'file_found_neither', len(file_found_neither)
 for f in file_found_neither:
     print '\t', f
-print 'file_found_multiple_matches'
-for f in file_found_multiple_matches:
-    print '\t', f
+
 
 
 # element = '<span data-id="dynamic-sku-price" class="product_price emphasis ">$287.<sup>99</sup></span>'
